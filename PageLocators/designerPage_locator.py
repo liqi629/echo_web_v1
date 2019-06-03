c2d33b6b602d71c0760fd432897bac698b247209
liqi629 <liqi_629@163.com>
2019-06-03 19:16:11 +0800

    更新元素定位

--- PageLocators/designerPage_locator.py
+++ PageLocators/designerPage_locator.py
@@ -6,10 +6,16 @@
 # @Software : PyCharm
 from selenium.webdriver.common.by import By
 class DesignerPageLocator:
+    #not_delete job
+    not_delete_job = (By.XPATH,'//span[@id="treeDemo_2_span"]')
     #新建作业+号
     add_job=(By.XPATH,'//img[@id="addBgBac"]')
     #作业名称输入框//input[@id="bisName"]
     job_name_input = (By.XPATH, '//input[@id="bisName"]')
+    #修改作业，名称输入
+    edit_name_input = (By.XPATH,'//input[@id="editBus"]')
+    #修改按钮
+    edit_name_button = (By.XPATH,'//a[@id="btn-edit"]')
     #新建作业确认按钮
     job_button = (By.XPATH, '//a[@id="btnBusinessAdd"]')
     #新建作业成功/名称重复toast提示//div[@class="toast-message"]
@@ -18,8 +24,10 @@ class DesignerPageLocator:
     job_list_new = (By.XPATH, ' //span[text()="auto_test_2222"]')
     # 编辑名称后的作业，列表中的定位 //span[text()="auto_test_3"]
     job_list_new_1 = (By.XPATH, ' //span[text()="auto_test_2223"]')
-    #鼠标悬浮新建作业，删除按钮//span[text()="删除"][1]
-    delete_job_button = (By.XPATH, '//span[text()="删除"][1]')
+    # 鼠标悬浮作业，编辑按钮
+    edit_job_button = (By.XPATH, '//span[@class="fa fa-edit btn-edit-business"]')
+    #鼠标悬浮新建作业，删除按钮
+    delete_job_button = (By.XPATH, '//span[@class="fa fa-trash btn-del-business"]')
     #删除作业弹窗 的删除按钮
     dialog_delete_button = (By.XPATH, '//a[@id="btn-delete"]')
 
@@ -29,8 +37,33 @@ class DesignerPageLocator:
     add_button = (By.XPATH, '//span[text()="添加"]')
     #保存按钮
     save_button = (By.XPATH,'//span[text()="保存"]')
+    #发布按钮
+    publish_button = (By.XPATH,'//span[text()="发布"]')
+    #本地运行按钮
+    run_locally = (By.XPATH,'//span[text()="本地运行"]')
+    #本不是运行按钮
+    run_distributed = (By.XPATH,'//span[text()="分布运行"]')
+
+
+
     #数据库，树加号，展开作用//span[@id="dataTree_1_switch"]
     data_tree_1 = (By.XPATH,'//span[@id="dataTree_1_switch"]')
+    #目标//span[text()="目标"]
+    target_source = (By.XPATH,'//span[text()="目标"]')
+    #映射关系
+    map = (By.XPATH,'//span[text()="映射关系"]')
+    #保存，映射配置弹出窗的确定按钮
+    map_config_button = (By.XPATH,'//a[@id="btn-relation-add"]')
+    #更新映射成功的toast：//div[text()="更新映射成功"]
+    toast_message = (By.XPATH,'//div[text()="更新映射成功"]')
+    #多表配置
+    table_config = (By.XPATH,'//span[text()="多表配置"]')
+    #多元配置弹出窗-确定按钮//a[@class="btn btn-primary btn-config-confirm"]
+    config_button = (By.XPATH,'//a[@class="btn btn-primary btn-config-confirm"]')
+    #表的连线
+    table_line = (By.CSS_SELECTOR,'html body div.container-fluid div.row div#skin_content.col-md-12.col-sm-12.col-lg-12.col-xs-12 div.content div#body div#skin-panel.panel.panel-default div.panel-body div#draw.draw-div div#graphContainer svg g g g path')
+
+
 
     #MySQL数据库，操作相关========================================================================
     #数据库-MySQL  //span[text()="MySQL"]
@@ -69,10 +102,10 @@ class DesignerPageLocator:
     last_step = (By.XPATH,'//a[text()="上一步"]')
     #下一步按钮//a[text()="下一步"]
     next_step = (By.XPATH,'//a[text()="下一步"]')
-    #数据库第一个表//input[@name="tableCheckList"][1]
-    first_tab = (By.XPATH,'//input[@name="tableCheckList"][1]')
+    #数据库第一个表
+    first_tab = (By.XPATH,'//input[@data-name="AUTO_test_01"]')
     #数据库第二个表
-    second_tab = (By.XPATH,'//input[@name="tableCheckList"][2]')
+    second_tab = (By.XPATH,'//input[@data-name="AUTO_test_02"]')
     #选表后的下一步
     next_step = (By.XPATH,'//a[text()="下一步"]')
     #选字段信息后的下一步
@@ -83,3 +116,5 @@ class DesignerPageLocator:
 
     #添加成功后的数据源
     source_title = (By.XPATH,'//span[text()="自动化测试数据源一"]')
+    # 添加成功后的目标源
+    out_source_title = (By.XPATH, '//span[text()="自动化测试目标源一"]')
